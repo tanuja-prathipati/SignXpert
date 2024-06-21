b4a4dd2c43d07cea9fb17f838dd3b20280c8c013
tanuja-prathipati <148349700+tanuja-prathipati@users.noreply.github.com>
2024-06-21 17:16:56 +0530

    21/06/24

--- app.py
+++ app.py
@@ -8,15 +8,16 @@ app = Flask(__name__)
 # Map of words to sign images
 SIGN_IMAGE_MAP = {
     "hello": "/static/sign_images/hello.png",
-    "day": "/static/sign_images/day.png",
     "friend": "/static/sign_images/friend.png",
-    "love": "/static/sign_images/love.png",
     "goodbye": "/static/sign_images/goodbye.png",
-    "no": "/static/sign_images/no.png",
-    "please": "/static/sign_images/please.png",
     "thank you": "/static/sign_images/thankyou.png",
-    "yes": "/static/sign_images/yes.png",
     "yesterday": "/static/sign_images/yesterday.png",
+    "tomorrow": "/static/sign_images/tomorrow.png",
+    "confession": "/static/sign_images/confession.png",
+    "afternoon": "/static/sign_images/afternoon.png",
+    "doctor": "/static/sign_images/doctor.png",
+    "interpreter": "/static/sign_images/interpreter.png",
+    "manager": "/static/sign_images/manager.png",
     # Add more mappings as needed
 }
 
